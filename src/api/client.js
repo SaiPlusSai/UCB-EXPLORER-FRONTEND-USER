0ab51d357e758c0b56c32ca7afd9cb517d2529b5
SaiPlusSai <refgu774@gmail.com>
2026-05-10 02:28:48 -0400

    Actualizacion
    Actualizacion

--- src/api/client.js
+++ src/api/client.js
@@ -1,17 +1,27 @@
 import axios from 'axios'
 
+console.log('MODE:', import.meta.env.MODE)
+console.log('API URL:', import.meta.env.VITE_API_URL)
+
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 export const api = axios.create({
   baseURL: `${baseURL}/api`,
   timeout: 20000,
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+  },
 })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('ucb_visitor_token')
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
+
+  config.headers['ngrok-skip-browser-warning'] = 'true'
+
   return config
 })
 
@@ -22,6 +32,7 @@ api.interceptors.response.use(
       localStorage.removeItem('ucb_visitor_token')
       localStorage.removeItem('ucb_visitor')
     }
+
     return Promise.reject(error)
   }
 )
